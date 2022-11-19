ae7d4a0aa2add4561174f23cb3f202bf8a2381c3
lima6615 <leo-queiroz1@outlook.com>
2022-11-19 14:28:32 -0300

    Database

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,104 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long id;
+	private LocalDate date;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	
+	public Sale() {
+	}
+	
+	public Sale(Long id, LocalDate date, String sellerName, Integer visited, Integer deals, Double amount) {
+		this.id = id;
+		this.date = date;
+		this.sellerName = sellerName;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsmeta.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SaleRepository {
+}
