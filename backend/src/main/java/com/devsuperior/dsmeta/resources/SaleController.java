5b65e263e1d225ab87350c75bf8fd7c2293b02ad
lima6615 <leo-queiroz1@outlook.com>
2022-12-16 10:48:06 -0300

    twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/resources/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/resources/SaleController.java
@@ -5,12 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.dto.SaleDTO;
 import com.devsuperior.dsmeta.service.SaleService;
+import com.devsuperior.dsmeta.service.SmsService;
 
 @RestController
 @RequestMapping (value = "/sales")
@@ -18,6 +20,9 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findSales(
@@ -27,5 +32,10 @@ public class SaleController {
 		
 		Page<SaleDTO> dto = service.findSales(minDate, maxDate, pageable);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@GetMapping("/{id}/{notifications}")
+	public void smsNotify(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }
